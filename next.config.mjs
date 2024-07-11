2f72b708ca8fd5208c84de410eb3ecec6541291a
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2024-07-11 15:23:27 +0530

    build error fixed 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true
+    appDir: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuild: true,
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
